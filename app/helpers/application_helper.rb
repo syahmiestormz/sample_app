6a06008d87ff3a83eb48abe80323335bf7a16b70
syahmie <syahmie2008@gmail.com>
2014-07-24 06:30:52 +0800

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+# Returns the full title on a per-page basis.
+ def full_title(page_title)
+   # Method definition
+ base_title = "Code Ar.my-X Sample App"
+ if page_title.empty?
+   # Boolean test
+ base_title
+ else
+ "#{base_title} | #{page_title}"
+ end
+ end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,11 +3,15 @@ describe "Static pages" do
 describe "Home page" do
 it "should have the content 'Sample App'" do
 visit '/static_pages/home'
-expect(page) .to have_content( 'Sample App' )
+expect(page).to have_content('Sample App')
 end
-it "should have the title 'Home'" do
+it "should have the base title" do
 visit '/static_pages/home'
-expect(page).to have_title("Code Ar.my-X Sample App | Home")
+expect(page).to have_title("Code Ar.my-X Sample App")
+end
+it "should not have a custom page title" do
+visit '/static_pages/home'
+expect(page).not_to have_title('| Home')
 end
 end
 end
@@ -18,7 +22,7 @@ describe "Static pages" do
 describe "Help page" do
 it "should have the content 'Help'" do
 visit '/static_pages/help'
-expect(page) .to have_content( 'Help' )
+expect(page).to have_content( 'Help' )
 end
 it "should have the title 'Help'" do
 visit '/static_pages/help'
@@ -33,7 +37,7 @@ describe "Static pages" do
 describe "About page" do
 it "should have the content 'About Us'" do
 visit '/static_pages/about'
-expect(page) .to have_content( 'About Us' )
+expect(page).to have_content( 'About Us' )
 end
 it "should have the title 'About Us'" do
 visit '/static_pages/about'
